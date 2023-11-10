3c080b2e57ee6007789200d3fd47abcd42c74ea5
nguyentranthevu <nguyentranthevu@gmail.com>
2023-11-10 21:01:38 +0700

    resposive: main page

--- components/Main/main.jsx
+++ components/Main/main.jsx
@@ -14,54 +14,51 @@ export default function Main(props) {
   };
   return (
     <>
-      <div className="col-start-6 col-span-2 mt-12 text-center">
-        <div className="font-bold text-[30px] uppercase ">{type}</div>
-      </div>
-      <>
-        <div className=" product col-start-3 col-span-8 mt-12 mb-6 cursor-pointer">
-          <div className="grid grid-cols-4 gap-4 m-auto text-center">
-            {data.slice(0, numItems).map((product, index) => {
-              return (
-                <div
-                  key={index}
-                  className="w-auto m-0 bg-white border border-slate-200 rounded hover:border-slate-300"
-                  style={{
-                    padding: "24px 5px 16px 5px",
-                    boxShadow: "0px 1px 8px rgba(0, 0, 0, 0.2)",
-                     maWidth: "300px",
-                  }}>
-                  <div className="product-item">
-                    <div className="thumnail" style={{ margin: "25px 0 20px" }}>
-                      <Link href={"/"}>
-                        <Image
-                          alt={product.name}
-                          src={product.image}
-                          width={240}
-                          height={240}
-                          style={{ objectFit: "scale-down" }}
-                        />
-                      </Link>
-                    </div>
-                     <div className="text-[16px] h-[2.5rem] font-bold leading-7 text-black">{product.name}</div>
-                      <div className="mt-6 price text-[20px] font-semibold text-red-600">
-                        <span>{NumberFormat(product.price)}</span>
-                        <span className="text-[16px] ml-1 mb-1">VNĐ</span>
-                      </div>
-
+      <div className=" product col-start-3  col-span-8 mt-12 mb-6 cursor-pointer text-center">
+        <div className="font-bold text-[30px] mb-10 ">{type}</div>
+        <div className="grid grid-cols-4 sm:grid-cols-1 md:grid-cols-2 gap-4 m-auto text-center">
+          {data.slice(0, numItems).map((product, index) => {
+            return (
+              <div
+                key={index}
+                className="w-auto m-0 bg-white border border-slate-200 rounded hover:border-slate-300"
+                style={{
+                  padding: "24px 5px 16px 5px",
+                  boxShadow: "0px 1px 8px rgba(0, 0, 0, 0.2)",
+                  maWidth: "300px",
+                }}>
+                <div className="product-item">
+                  <div className="thumnail" style={{ margin: "25px 0 20px" }}>
+                    <Link href={"/"}>
+                      <Image
+                        alt={product.name}
+                        src={product.image}
+                        width={240}
+                        height={240}
+                        style={{ objectFit: "scale-down" }}
+                      />
+                    </Link>
+                  </div>
+                  <div className="text-[16px] h-[2.5rem] font-bold leading-7 text-black">{product.name}</div>
+                  <div className="mt-6 price text-[20px] font-semibold text-red-600">
+                    <span>{NumberFormat(product.price)}</span>
+                    <span className="text-[16px] ml-1 mb-1">VNĐ</span>
                   </div>
                 </div>
-              );
-            })}
-          </div>
+              </div>
+            );
+          })}
         </div>
         {!loadMore && (
-          <div className="col-start-6 col-span-2 mt-3" onClick={() => handleClick()}>
-            <button className=" min-w-[150px] w-full px-8 py-4 rounded-lg border border-blue-500 text-blue-500 hover:bg-slate-200">
-              Xem Thêm {type}
-            </button>
+          <div className="grid grid-cols-5 md:grid-cols-1 sm:grid-cols-1">
+            <div className="lg:col-start-3  mt-12 text-center mx-auto" onClick={() => handleClick()}>
+              <div className="min-w-[150px] w-full px-8 py-4 rounded-lg border border-blue-500 text-blue-500 hover:bg-slate-200">
+                Xem Thêm {type}
+              </div>
+            </div>
           </div>
         )}
-      </>
+      </div>
     </>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,11 +8,20 @@ module.exports = {
   ],
   theme: {
     screens: {
-      'sm': '640px',
-      'md': '768px',
-      'lg': '1024px',
-      'xl': '1280px',
-      '2xl': '1536px',
+      sm: { max: "640px" },
+      // => @media (max-width: 640px and max-width: 767px) { ... }
+
+      md: { min: "645px", max: "800px" },
+      // => @media (max-width: 768px and max-width: 1023px) { ... }
+
+      lg: { min: "1024px" },
+      // => @media (max-width: 1024px and max-width: 1279px) { ... }
+
+      xl: { min: "1280px" },
+      // => @media (min-width: 1280px and max-width: 1535px) { ... }
+
+      "2xl": { min: "1536px" },
+      // => @media (min-width: 1536px) { ... }
     },
     extend: {
       backgroundImage: {
